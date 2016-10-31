e6dc18e09f11f68f4a5bade6ef208b3746364822
PDH <pdhuguenin@gmail.com>
2016-10-31 16:03:30 -0600

    Current Update

---
+++ credit_check_new.rb
@@ -0,0 +1,22 @@
+credit_card_num = "4929735477250543"
+credit_card_num.reverse!
+
+#set each digit to a variable
+number_1 = credit_card_num[0].to_i
+number_2 = credit_card_num[1].to_i
+number_3 = credit_card_num[2].to_i
+number_4 = credit_card_num[3].to_i
+number_5 = credit_card_num[4].to_i
+number_6 = credit_card_num[5].to_i
+number_7 = credit_card_num[6].to_i
+number_8 = credit_card_num[7].to_i
+number_9 = credit_card_num[8].to_i
+number_10 = credit_card_num[9].to_i
+number_11 = credit_card_num[10].to_i
+number_12 = credit_card_num[11].to_i
+number_13 = credit_card_num[12].to_i
+number_14 = credit_card_num[13].to_i
+number_15 = credit_card_num[14].to_i
+number_16 = credit_card_num[15].to_i
+
+s1 = (number_1 + number_3 + number_5 + number_7  + number_9 + number_11 + number_13 + number_15)
